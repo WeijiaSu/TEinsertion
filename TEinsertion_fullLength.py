23912574e02d080177a083e14df0800770e417c0
weijiasu <weijiasu0804@gmail.com>
2023-01-20 18:23:41 -0500

    fl

--- TEinsertion_fullLength.py
+++ TEinsertion_fullLength.py
@@ -18,7 +18,6 @@ pd.set_option("display.max_column",40)
 
 parser=argparse.ArgumentParser()
 parser.add_argument("-Ta","--TE_bam")
-parser.add_argument("-Ga","--Genome_bam")
 parser.add_argument("-pName","--Prefix")
 parser.add_argument("-fq","--Rawfastq")
 parser.add_argument("-genome","--Genome")
@@ -27,30 +26,25 @@ parser.add_argument("-flex","--flexibility",default=100)
 args=parser.parse_args()
 
 Ta=args.TE_bam
-Ga=args.Genome_bam
 pName=args.Prefix
 fq=args.Rawfastq
 fl=args.flexibility
 genome=args.Genome
 
-def FullLegnthTE(TEfile):
-	# Read paf of TE and the genome alignment
-	TE=pd.read_table(TEfile,header=None,sep=" ")
-	# Get the first 9 columns of both files.
-	TE=TE[range(0,9)]
-	# Re-name the columns
-	TE_columns=["Readname","ReadLen","ReadStart_TE","ReadEnd_TE","Strand_TE","TE_Name","TElen","TEstart","TEend"]
-	TE.columns=TE_columns
+def FullLegnthTE(TE):
 	
 	#Select full length
 	TE=TE.loc[(TE["TEstart"]<=fl) & (TE["TEend"]>=TE["TElen"]-fl)]
 	TE=TE.loc[(TE["ReadStart_TE"]>=100) | (TE["ReadEnd_TE"]<=TE["ReadLen"]-100)]
-	left=TE[["Readname","ReadStart_TE"]]
-	left["s"]=0
-	left=left[["Readname","s","ReadStart_TE"]]
-	right=TE[["Readname","ReadEnd_TE","ReadLen"]]
-	left.to_csv(pName+".left",header=None,index=None,sep="\t")
-	right.to_csv(pName+".right",header=None,index=None,sep="\t")
+	if TE.shape[0]>0:
+		left=TE[["Readname","ReadStart_TE"]]
+		left["s"]=0
+		left=left[["Readname","s","ReadStart_TE"]]
+		right=TE[["Readname","ReadEnd_TE","ReadLen"]]
+		left.to_csv(pName+".left",header=None,index=None,sep="\t")
+		right.to_csv(pName+".right",header=None,index=None,sep="\t")
+	else:
+		print("No full length %s"%(list(TE["TE_Name"])[0]))
 	return TE
 
 def getSeq(fastq,Genome):
@@ -62,7 +56,6 @@ def getSeq(fastq,Genome):
 	os.system(minimap)
 	minimap="minimap2 -x map-ont %s %s -Y -t 16 | awk '{for(i=1;i<=9;i++) printf $i\" \";print \"\"}' FS='\\t' > %s"%(Genome,pName+".right.fastq",pName+".right.paf")
 	os.system(minimap)
-	print(minimap)
 
 def getJuctions(paf1,paf2):
 	left=pd.read_table(paf1,header=None,sep=" ")
@@ -87,7 +80,9 @@ def getJuctions(paf1,paf2):
 	combined.loc[combined["4_y"]=="+","J2"]=combined["7_y"]
 	combined.loc[combined["4_y"]=="-","J2"]=combined["8_y"]
 	combined=combined.loc[abs(combined["J1"]-combined["J2"])<1000]
-	combined.to_csv(pName+".insReads.tsv",header=None,index=None,sep="\t")
+	if combined.shape[0]>0:
+		combined.to_csv(pName+".insReads.tsv",header=None,index=None,sep="\t")
+	return combined
 
 def Cluster(combined_result):
 	f=pd.read_table(combined_result,header=None)
@@ -103,7 +98,6 @@ def Cluster(combined_result):
 	bedInput.to_csv(pName+"bedInput.tsv",header=None,index=None,sep="\t")
 	bedtools="bedtools cluster -i %s -d 1000 > %s"%(pName+"bedInput.tsv",pName+"cluster.tsv")
 	os.system(bedtools)
-	os.remove(pName+"bedInput.tsv")
 
 
 def Assign(Cluster,combined,fl_TE):
@@ -126,17 +120,38 @@ def Assign(Cluster,combined,fl_TE):
 	fl_TE=fl_TE[["Readname","TE_Name"]]
 	res=f.merge(fl_TE,on=["Readname"],how="inner")
 	res=res.sort_values(["cluster"])
-	print(res[0:10])
-	print(res.shape)
-
-Assign(pName+"cluster.tsv",pName+".insReads.tsv",full_TE)
+	return res
 
 
-def Main():
-	full_TE=FullLegnthTE(Ta)
-	getSeq(fq,genome)
-	getJuctions(pName+".left.paf",pName+".right.paf")
-	Cluster(pName+".insReads.tsv")
-	Assign(pName+"cluster.tsv",pName+".insReads.tsv",full_TE)
 
+def Main(TEmappingDf):
+	full_TE=FullLegnthTE(TEmappingDf)
+	if full_TE.shape[0]>0:
+		getSeq(fq,genome)
+		com=getJuctions(pName+".left.paf",pName+".right.paf")
+		if com.shape[0]>0:
+			Cluster(pName+".insReads.tsv")
+			TEres=Assign(pName+"cluster.tsv",pName+".insReads.tsv",full_TE)
+			os.remove(pName+".left")
+			os.remove(pName+".right")
+			os.remove(pName+".left.fastq")
+			os.remove(pName+".right.fastq")
+			os.remove(pName+".left.paf")
+			os.remove(pName+".right.paf")
+			os.remove(pName+".insReads.tsv")
+			os.remove(pName+"bedInput.tsv")
+			os.remove(pName+"cluster.tsv")
+			return TEres
+
+final_res=pd.DataFrame(columns=["Readname","REFname","REFstart","REFend","cluster","TE_Name"])
+TEmap=pd.read_table(Ta,header=None,sep=" ")
+TEmap=TEmap[range(0,9)]
+TEmap_columns=["Readname","ReadLen","ReadStart_TE","ReadEnd_TE","Strand_TE","TE_Name","TElen","TEstart","TEend"]
+TEmap.columns=TEmap_columns
+for i in set(TEmap["TE_Name"]):
+	sub=TEmap.loc[TEmap["TE_Name"]==i]
+	res=Main(sub)
+	final_res=final_res.append(res)
+
+final_res.to_csv(pName+"_fullLen_insertion.tsv",index=None,sep="\t")
 
