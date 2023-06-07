e6fbe473eebaab7ef4a42d6fa2b64c4688b8b316
weijiasu <weijiasu0804@gmail.com>
2023-06-07 16:42:42 -0400

    add

--- TEinsertion_FL.sh
+++ TEinsertion_FL.sh
@@ -6,9 +6,9 @@ ref=$3
 outPre=$4
 
 readName=$(basename $reads);
-#porechop -i $reads --extra_end_trim 0 --discard_middle -o $readName".pre.fastq"
+porechop -i $reads --extra_end_trim 0 --discard_middle -o $readName".pre.fastq"
 refName=$(basename $ref);
 TEname=$(basename $TE)
 minimap2 -x map-ont $TE $readName".pre.fastq" -Y -t 16 | awk '{for(i=1;i<=9;i++) printf $i" "; print ""}' FS='\t' > $readName"_"$TEname".paf";
-python3 /data/zhanglab/Weijia_Su/Git/TEinsertion/TEinsertion_fullLength.py -Ta $readName"_"$TEname".paf" -fq $readName".pre.fastq" -genome $ref -pName $outPre
+#python3 /data/zhanglab/Weijia_Su/Git/TEinsertion/TEinsertion_fullLength.py -Ta $readName"_"$TEname".paf" -fq $readName".pre.fastq" -genome $ref -pName $outPre
 
