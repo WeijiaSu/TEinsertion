132f44a5cca4cbbd7505f662d0d1d750543e529d
weijiasu <weijiasu0804@gmail.com>
2023-06-12 13:55:30 -0400

    update

--- TEinsertion_FL.sh
+++ TEinsertion_FL.sh
@@ -10,5 +10,5 @@ porechop -i $reads --extra_end_trim 0 --discard_middle -o $readName".pre.fastq"
 refName=$(basename $ref);
 TEname=$(basename $TE)
 minimap2 -x map-ont $TE $readName".pre.fastq" -Y -t 16 | awk '{for(i=1;i<=9;i++) printf $i" "; print ""}' FS='\t' > $readName"_"$TEname".paf";
-#python3 /data/zhanglab/Weijia_Su/Git/TEinsertion/TEinsertion_fullLength.py -Ta $readName"_"$TEname".paf" -fq $readName".pre.fastq" -genome $ref -pName $outPre
+python3 /data/zhanglab/Weijia_Su/Git/TEinsertion/TEinsertion_fullLength.py -Ta $readName"_"$TEname".paf" -fq $readName".pre.fastq" -genome $ref -pName $outPre
 
