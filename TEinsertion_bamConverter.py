595aaa1186e05241bbf596534df2890bc94d6e1d
weijiasu <weijiasu0804@gmail.com>
2023-06-12 15:50:33 -0400

    update

--- TEinsertion_bamConverter.py
+++ TEinsertion_bamConverter.py
@@ -13,8 +13,6 @@ import pysam
 import warnings
 
 warnings.filterwarnings('ignore')
-
-
 pd.set_option("display.max_column",40)
 
 #parser=argparse.ArgumentParser()
@@ -26,33 +24,34 @@ pd.set_option("display.max_column",40)
 class bamConverter:
 
 	#Conver bam to bed file
-	def ConverAlignment(self,bam):
-	  index="samtools index %s"%(bam)
-	  os.system(index)
-	  samfile = pysam.AlignmentFile(bam, "rc")
-	  f=open(bam+"_AligTable.tsv","w")
-	  columns=["Refname","RefStart","RefEnd","Readname","ReadLen","ReadStart","ReadEnd","Strand"]
-	  f.write("\t".join(columns)+"\n")
-	  for read in samfile.fetch():
-	    if read.is_unmapped==False:
-	      Refname=read.reference_name
-	      RefStart=read.reference_start+1
-	      RefEnd=read.reference_end
-	      Readname=read.query_name
-	      ReadLen=read.query_length
-	
-	      r1=read.query_alignment_start+1
-	      r2=read.query_alignment_end
-	      if read.is_reverse:
-	        Strand="-"
-	        ReadStart=ReadLen-r2+1
-	        ReadEnd=ReadLen-r1+1
-	      else:
-	        Strand="+"
-	        ReadStart=r1
-	        ReadEnd=r2
-	      l=[Refname,RefStart,RefEnd,Readname,ReadLen,ReadStart,ReadEnd,Strand]
-	      l=[str(i) for i in l]
-	      s="\t".join([str(i) for i in l]) 
-	      f.write(s+"\n")
-	  f.close()
+	def ConverAlignment(self,bam):\
+		index="samtools index %s"%(bam)
+		os.system(index)
+		samfile = pysam.AlignmentFile(bam, "rc")
+		f=open(bam+"_AligTable.tsv","w")
+		columns=["QName","QLen","QStart","QEnd","Strand","RName","RLen","RStart","REnd"]
+		f.write("\t".join(columns)+"\n")
+		for read in samfile.fetch():
+			if read.is_unmapped==False:
+				Refname=read.reference_name
+				RefStart=read.reference_start+1
+				RefEnd=read.reference_end
+				RefLen=samfile.get_reference_length(Refname)
+				Readname=read.query_name
+				ReadLen=read.query_length
+				r1=read.query_alignment_start+1
+				r2=read.query_alignment_end
+				if read.is_reverse:
+					Strand="-"
+					ReadStart=ReadLen-r2+1
+					ReadEnd=ReadLen-r1+1
+				else:
+					Strand="+"
+					ReadStart=r1
+					ReadEnd=r2
+				l=[]
+				l=[Refname,RefStart,RefEnd,Readname,ReadLen,ReadStart,ReadEnd,Strand]
+				l=[str(i) for i in l]
+				s="\t".join([str(i) for i in l]) 
+				f.write(s+"\n")
+		f.close()
