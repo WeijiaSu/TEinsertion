a17afe356c5f59f07c94e641b75094952851fd9a
weijiasu <weijiasu0804@gmail.com>
2023-06-13 09:54:46 -0400

    update

--- Draft.py
+++ Draft.py
@@ -17,9 +17,12 @@ pd.set_option("display.max_columns",40)
 
 
 f1=pd.read_table("/data/zhanglab/Weijia_Su/TestInsertion/test_061223/shmCherry.fastq-TE_full.fa.bam_AligTable.tsv")
-print(f1[0:10])
+f1=f1.loc[f1["Strand"]=="-"]
+print(f1[0:20])
 print(f1.shape)
 
 f2=pd.read_table("/data/zhanglab/Weijia_Su/TestInsertion/test_061223/shmCherry.fastq-TE_full.fa.bam.paf")
-print(f2[0:10])
+f2=f2.loc[f2["Strand"]=="-"]
+print(f2[0:20])
 print(f2.shape)
+
