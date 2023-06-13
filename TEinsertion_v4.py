a553c02f636402c3fc14c08db4af8caca2d78090
weijiasu <weijiasu0804@gmail.com>
2023-06-13 10:40:44 -0400

    update v4

--- TEinsertion_v4.py
+++ TEinsertion_v4.py
@@ -18,20 +18,31 @@ warnings.filterwarnings('ignore')
 pd.set_option("display.max_column",40)
 
 parser=argparse.ArgumentParser()
-parser.add_argument("-t","--TE_bam",description='bam file with reads mapped to transposon consensus sequences')
-Ta=args.TE_bam
-TaName=str(Ta.split("/")[-1])
-parser.add_argument("-n","--Name",description="Prefix",default=TaName)
+parser.add_argument("-t","--TE_bam",help='bam file with reads mapped to transposon consensus sequences')
+parser.add_argument("-n","--Name",help="Prefix")
 parser.add_argument("-fq","--Rawfastq")
-parser.add_argument("-genome","--Genome")
+parser.add_argument("-g","--Genome")
 parser.add_argument("-flex","--flexibility",default=100)
 
 args=parser.parse_args()
 
 Ta=args.TE_bam
-pName=args.Prefix
+pName=args.Name
 fq=args.Rawfastq
 fl=args.flexibility
 genome=args.Genome
 
 bamConverter().ConverAlignment(Ta)
+
+def getMappedReads(bam):
+	samtools="samtools view -b -F 4 %s | samtools fastq > %s"%(bam,pName+".mappedTE.fastq")
+	os.system(samtools)
+
+
+
+def MapToGenome():
+	minimap2="minimap2 -ax map-ont %s %s -Y -t 16 | samtools view -bS | samtools sort > %s"%(genome,pName+".mappedTE.fastq",pName+"_genome.bam")
+	os.system(minimap2)
+
+#getMappedReads(Ta)
+MapToGenome()
